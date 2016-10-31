ed47a4be3455fdf0d8598c9af43a9eea21c23161
ansuz <ansuz@transitiontech.ca>
2016-10-31 14:46:02 +0100

    very WIP

--- index.js
+++ index.js
@@ -1,10 +1,65 @@
 var Dnsd = require("dnsd");
 var Client = require("ssb-client");
+var Pull = require("pull-stream");
+var Paramap = require("pull-paramap");
 
-var createServer = function (port, host, cb) {
+var log = {
+    req: function (req) {
+        var q = req.question[0];
+        console.log({
+            name: q.name,
+            type: q.type,
+            class: q.class,
+        });
+    },
+};
+
+var answer = function (sbot, req, res) {
+    console.log();
+    log.req(req);
+
+    var q = req.question;
+
+    if (!q.length) {
+        console.log("invalid question");
+        res.end();
+    }
+
+    var name = q[0].name;
+    var type = q[0].type;
+
+    Pull(sbot.messagesByType({
+        type: 'ssb-dns',
+    }),
+    Paramap(function getAvatar(msg, cb) {
+        cb(null, msg);
+    }),
+    Pull.drain(function printMessage(msg) {
+        var text = msg.value.content.text;
+
+        var result;
+        try {
+            result = JSON.parse(text);
+        } catch (err) {
+            throw new Error("Invalid record!");
+        }
+        if (name && type && result.name === name && result.type === type && result.value) {
+            console.log("%s (%s) => %s", name, result.type, result.value);
+            res.answer.push({
+                name: result.name,
+                type: result.type,
+                data: result.value,
+                ttl: 500,
+            });
+            res.end()
+        }
+    }));
+};
+
+var createServer = function (sbot, port, host, cb) {
     Dnsd.createServer(function(req, res) {
-        res.end('1.2.3.4')
-    }).listen(port, host, cb)
+        answer(sbot, req, res);
+    }).listen(port, host, cb);
 };
 
 Client(function (err, sbot) {
@@ -14,9 +69,8 @@ Client(function (err, sbot) {
     }
     var port = 53053;
     var host = '127.0.0.1';
-    createServer(port, host, function () {
+    createServer(sbot, port, host, function () {
         console.log('Server running at %s:%s', host, port);
     });
 });
-
 
