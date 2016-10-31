b98793c4e8e78416faf3d283fda75768912aa7ba
ansuz <ansuz@transitiontech.ca>
2016-10-31 13:58:55 +0100

    publish an ssb-dns record with ./publish.js

---
+++ publish.js
@@ -0,0 +1,79 @@
+#!/bin/sh
+':' //; exec "$(command -v nodejs || command -v node)" "$0" "$@"
+// http://unix.stackexchange.com/questions/65235/universal-node-js-shebang
+
+
+var args = process.argv.slice(2);
+
+if (args.length < 3 || args.length > 4) {
+    console.log("Try: ");
+    console.log("./publish.js name type value (class)");
+    process.exit(1);
+}
+
+var TYPES = ['A', 'AAAA', 'CNAME', 'HINFO', 'ISDN', 'MX', 'NS', 'PTR', 'SOA', 'TXT'];
+
+var isValidType = function (t) {
+    return TYPES.indexOf(t) !== -1;
+};
+
+var CLASSES = ['IN', 'CH'];
+
+var isValidClass = function (c) {
+    return CLASSES.indexOf(c) !== -1;
+};
+
+var endsInSSB = function (s) {
+    return /\.ssb$/i.test(s);
+};
+
+var isValidRecord = function (record) {
+    return endsInSSB(record.name) &&
+        record.value &&
+        isValidType(record.type) &&
+        isValidClass(record.class);
+};
+
+var makeRecord = function (name, type, value, _class) {
+    return {
+        name: name,
+        type: type,
+        class: _class || 'IN',
+        value: value
+    };
+};
+
+var serialize = function (record) {
+    return JSON.stringify(record);
+};
+
+var publish = function (name, type, _class) {
+    var record = makeRecord(name, type, _class);
+
+    if (!isValidRecord(record)) {
+        console.error("invalid input");
+        return void process.exit(1);
+    }
+
+    require("ssb-client")(function (err, sbot) {
+        if (err) {
+            console.error(err);
+            return void process.exit(1);
+        }
+
+        // publish a message
+        sbot.publish({ type: 'ssb-dns', text: serialize(record) }, function (err, msg) {
+
+            console.log(msg);
+
+            console.log(JSON.stringify(record, null, 4));
+            sbot.close();
+            // msg.key           == hash(msg.value)
+            // msg.value.author  == your id
+            // msg.value.content == { type: 'post', text: 'My First Post!' }
+            // ...
+        })
+    });
+};
+
+publish(args[0], args[1], args[2]);
