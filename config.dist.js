74fa29c449dc7bf9f7edae3702d044a84873b986
ansuz <ansuz@transitiontech.ca>
2016-10-31 23:55:34 +0100

    update readme. remove config file. bump version

--- config.dist.js
+++
@@ -1,8 +0,0 @@
-var Config = module.exports = {
-    verbose: true,
-    port: 53053,
-    host: '127.0.0.1',
-    ready: function () {
-        console.log("\nServer running at %s:%s", Config.host, Config.port);
-    },
-};
